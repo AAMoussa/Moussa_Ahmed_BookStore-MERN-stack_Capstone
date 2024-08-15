a8be1f9680095db01b7ef22d2c848e7ecf88a3f7
Ahmed Moussa <a.moussa0013@outlook.com>
2024-08-14 20:55:35 -0400

    Setting up the config module for defining PORT, localhost, and my DB URL string

--- backend/config.js
+++ backend/config.js
@@ -1,3 +1,9 @@
+// My URL for testing the server
+// http://localhost:5555
+
 //EXPORT IT AND IMPORT IT IN THE INDEX.JS FILE
 export const PORT = 5555; 
+
+//Create a variable for the DB connection URL
+export const mongoDBURL = "mongodb+srv://ahmedmoussa0013:AMmdb2013@cluster0.nola0m8.mongodb.net/Books-Store-MERN"
 
